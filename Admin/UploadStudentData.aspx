55e32ae0934180f208e328c405668c54a844af3c
Punj Prakash <129642471+punjprakashpp@users.noreply.github.com>
2024-11-12 08:08:22 +0530

    Upload Student Data

--- Admin/UploadStudentData.aspx
+++ Admin/UploadStudentData.aspx
@@ -1,34 +1,14 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="MasterPage.master" AutoEventWireup="true" CodeFile="UploadStudentData.aspx.cs" Inherits="Library_Librarian_UploadStudentData" %>
+﻿<%@ Page Title="Upload Student Data" Language="C#" MasterPageFile="Admin.master" AutoEventWireup="true" CodeFile="UploadStudentData.aspx.cs" Inherits="UploadStudentData" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
-    <title>Upload Student Data</title>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="Server">
+    <link rel="stylesheet" href="Styles/manage.css">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
-    <table class="tbl">
-        <tr>
-            <td class="tblhead">UPLOAD STUDENT DATA</td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-        </tr>
-        <tr>
-            <td>
-                <table align="center" class="style1">
-                    <tr>
-                        <td>
-                            <asp:FileUpload ID="FileUpload1" runat="server" CssClass="txt" />
-                            <asp:Button ID="Button1" runat="server" Text="Upload" OnClick="Button1_Click" CssClass="btn" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;</td>
-                    </tr>
-                     <tr>
-                        <td><asp:Label ID="lblmsg" runat="server"></asp:Label></td>
-                    </tr>
-                </table>
-            </td>
-        </tr>
-    </table>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="Server">
+    <div class="container">
+        <h2>Upload Student Data</h2>
+        <asp:FileUpload ID="fileUpload" runat="server" CssClass="txt" />
+        <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click" CssClass="btn" /><br />
+        <asp:Label ID="lblmsg" runat="server"></asp:Label>
+    </div>
 </asp:Content>
 
--- Admin/UploadStudentData.aspx.cs
+++ Admin/UploadStudentData.aspx.cs
@@ -5,108 +5,112 @@ using System.Configuration;
 using System.IO;
 using ClosedXML.Excel;
 
-public partial class Library_Librarian_UploadStudentData : System.Web.UI.Page
+public partial class UploadStudentData : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+    protected void btnUpload_Click(object sender, EventArgs e)
     {
-
-    }
-
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-        if (FileUpload1.HasFile)
+        if (fileUpload.HasFile)
         {
-            string filePath = Server.MapPath("~/docs/Uploads/") + Path.GetFileName(FileUpload1.PostedFile.FileName);
-            FileUpload1.SaveAs(filePath);
+            try
+            {
+                string filePath = Server.MapPath("~/Uploads/") + Path.GetFileName(fileUpload.PostedFile.FileName);
+                fileUpload.SaveAs(filePath);
 
-            DataTable dtExcelData = new DataTable();
-            dtExcelData.Columns.AddRange(new DataColumn[13] {
+                DataTable dtExcelData = new DataTable();
+                dtExcelData.Columns.AddRange(new DataColumn[10] {
                     new DataColumn("Session", typeof(string)),
-                    new DataColumn("Roll", typeof(string)),
-                    new DataColumn("Name", typeof(string)),
-                    new DataColumn("Mobile", typeof(string)),
-                    new DataColumn("Address", typeof(string)),
-                    new DataColumn("City", typeof(string)),
-                    new DataColumn("Pincode", typeof(string)),
+                    new DataColumn("RollNo", typeof(string)),
+                    new DataColumn("RegNo", typeof(string)),
+                    new DataColumn("RegYear", typeof(string)),
+                    new DataColumn("FirstName", typeof(string)),
+                    new DataColumn("MidName", typeof(string)),
+                    new DataColumn("LastName", typeof(string)),
                     new DataColumn("DOB", typeof(DateTime)),
                     new DataColumn("Gender", typeof(string)),
-                    new DataColumn("Email", typeof(string)),
-                    new DataColumn("Password", typeof(string)),
-                    new DataColumn("ImagePath", typeof(string)),
                     new DataColumn("EntryDate", typeof(DateTime))
                 });
 
-            using (var workbook = new XLWorkbook(filePath))
-            {
-                var worksheet = workbook.Worksheet(1);
-                bool firstRow = true;
-
-                foreach (var row in worksheet.Rows())
+                using (var workbook = new XLWorkbook(filePath))
                 {
-                    if (firstRow)
+                    var worksheet = workbook.Worksheet(1); // Assuming first sheet
+                    bool firstRow = true;
+
+                    foreach (var row in worksheet.Rows())
                     {
-                        firstRow = false;
-                        continue;
+                        if (firstRow)
+                        {
+                            firstRow = false;
+                            continue; // Skip header row
+                        }
+
+                        // Read data from Excel row
+                        string session = row.Cell(1).Value.ToString();
+                        string rollNo = row.Cell(2).Value.ToString();
+                        string regNo = row.Cell(3).Value.ToString();
+                        string regYear = row.Cell(4).Value.ToString();
+                        string firstName = row.Cell(5).Value.ToString();
+                        string midName = row.Cell(6).Value.ToString();
+                        string lastName = row.Cell(7).Value.ToString();
+                        string dobString = row.Cell(8).Value.ToString();
+                        string gender = row.Cell(9).Value.ToString();
+                        string entryDateString = row.Cell(13).Value.ToString();
+
+                        DateTime dob;
+                        DateTime entryDate;
+
+                        // Try parsing dates
+                        if (!DateTime.TryParse(dobString, out dob))
+                        {
+                            dob = DateTime.MinValue; // Default if invalid date
+                        }
+
+                        if (!DateTime.TryParse(entryDateString, out entryDate))
+                        {
+                            entryDate = DateTime.MinValue; // Default if invalid date
+                        }
+
+                        // Add the row to DataTable
+                        dtExcelData.Rows.Add(session, rollNo, regNo, regYear, firstName, midName, lastName, dob, gender, entryDate);
                     }
+                }
 
-                    string session = row.Cell(1).Value.ToString();
-                    string roll = row.Cell(2).Value.ToString();
-                    string name = row.Cell(3).Value.ToString();
-                    string mobile = row.Cell(4).Value.ToString();
-                    string address = row.Cell(5).Value.ToString();
-                    string city = row.Cell(6).Value.ToString();
-                    string pincode = row.Cell(7).Value.ToString();
-                    string dobString = row.Cell(8).Value.ToString();
-                    string gender = row.Cell(9).Value.ToString();
-                    string email = row.Cell(10).Value.ToString();
-                    string password = row.Cell(11).Value.ToString();
-                    string imagePath = row.Cell(12).Value.ToString();
-                    string entryDateString = row.Cell(13).Value.ToString();
-
-                    DateTime dob;
-                    DateTime entryDate;
-
-                    if (!DateTime.TryParse(dobString, out dob))
+                // SQL connection and bulk copy operation
+                string consString = ConfigurationManager.ConnectionStrings["WebsiteConnectionString"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(consString))
+                {
+                    using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
                     {
-                        dob = DateTime.MinValue;
-                    }
+                        sqlBulkCopy.DestinationTableName = "dbo.Student";
 
-                    if (!DateTime.TryParse(entryDateString, out entryDate))
-                    {
-                        entryDate = DateTime.MinValue;
-                    }
+                        // Column mappings (ensure the column names in database match exactly)
+                        sqlBulkCopy.ColumnMappings.Add("Session", "Session");
+                        sqlBulkCopy.ColumnMappings.Add("RollNo", "Roll");
+                        sqlBulkCopy.ColumnMappings.Add("RegNo", "RegNo");
+                        sqlBulkCopy.ColumnMappings.Add("RegYear", "RegYear");
+                        sqlBulkCopy.ColumnMappings.Add("FirstName", "FirstName");
+                        sqlBulkCopy.ColumnMappings.Add("MidName", "MidName");
+                        sqlBulkCopy.ColumnMappings.Add("LastName", "LastName");
+                        sqlBulkCopy.ColumnMappings.Add("DOB", "DOB");
+                        sqlBulkCopy.ColumnMappings.Add("Gender", "Gender");
+                        sqlBulkCopy.ColumnMappings.Add("EntryDate", "EntryDate");
 
-                    dtExcelData.Rows.Add(session, roll, name, mobile, address, city, pincode, dob, gender, email, password, imagePath, entryDate);
+                        con.Open();
+                        sqlBulkCopy.WriteToServer(dtExcelData);
+                        lblmsg.Text = "Student Data Added Successfully!";
+                        lblmsg.ForeColor = System.Drawing.Color.Green;  // Success message color
+                    }
                 }
             }
-
-            string consString = ConfigurationManager.ConnectionStrings["WebsiteConnectionString"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(consString))
+            catch (Exception ex)
             {
-                using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
-                {
-                    sqlBulkCopy.DestinationTableName = "dbo.Student";
-
-                    sqlBulkCopy.ColumnMappings.Add("Session", "Session");
-                    sqlBulkCopy.ColumnMappings.Add("Roll", "Roll");
-                    sqlBulkCopy.ColumnMappings.Add("Name", "Name");
-                    sqlBulkCopy.ColumnMappings.Add("Mobile", "Mobile");
-                    sqlBulkCopy.ColumnMappings.Add("Address", "Address");
-                    sqlBulkCopy.ColumnMappings.Add("City", "City");
-                    sqlBulkCopy.ColumnMappings.Add("Pincode", "Pincode");
-                    sqlBulkCopy.ColumnMappings.Add("DOB", "DOB");
-                    sqlBulkCopy.ColumnMappings.Add("Gender", "Gender");
-                    sqlBulkCopy.ColumnMappings.Add("Email", "Email");
-                    sqlBulkCopy.ColumnMappings.Add("Password", "Password");
-                    sqlBulkCopy.ColumnMappings.Add("ImagePath", "ImagePath");
-                    sqlBulkCopy.ColumnMappings.Add("EntryDate", "EntryDate");
-
-                    con.Open();
-                    sqlBulkCopy.WriteToServer(dtExcelData);
-                    lblmsg.Text = "Student Data Added Successfully !!";
-                    con.Close();
-                }
+                lblmsg.Text = "Error: " + ex.Message; // Display error message if any
+                lblmsg.ForeColor = System.Drawing.Color.Red;  // Error message color
             }
+        }
+        else
+        {
+            lblmsg.Text = "Please upload a file!";
+            lblmsg.ForeColor = System.Drawing.Color.Red;  // Error message color
         }
     }
 }
